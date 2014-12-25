a4e0a4c819de85a9f68cc5eee9d73f533be7fc21
Rohit Shetty <roit.shetty@gmail.com>
2014-12-26 02:54:57 +0530

    Fixing up old typos and documnented

--- deck.ino
+++ deck.ino
@@ -1,6 +1,12 @@
 #include <NewPing.h>
 
-
+/*
+PIN2 enable always keep high
+PIN4 to motor drive input
+PIN7 Another motor drive input
+PIN4, PIN7 =(1,0) IN deck
+PIN4, PIN7 =(0,1) OUT Deck
+*/
 
 #define TRIGGER_PIN 12
 #define ECHO_PIN    11
@@ -9,7 +15,7 @@
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
 void setup(){
-Serial.begin(9600);
+Serial.begin(9600); //For debuggin 
 
 pinMode(2,OUTPUT);
 pinMode(4,OUTPUT);
@@ -22,16 +28,14 @@ void loop(){
 delay(50);
 unsigned int uS=sonar.ping();
 unsigned dist = uS /US_ROUNDTRIP_CM;
-Serial.println(dist);
-if(dist >= 3 && dist < 20 ){
+Serial.println(dist); //Debug em all 
+if(dist >= 6 && dist < 20 ){
 out();
-
-}else if (dist < 3){
+}else if (dist < 4 and dist > 0.1 ){
   in();
-  
 }
 else{
-
+//This is Safe Zone
 }
 }
 
